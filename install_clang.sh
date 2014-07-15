f58df31313f9864159383b1b0f9ae1a9cf1d7874
Martin Zenzes <martin.zenzes@dfki.de>
2014-07-15 17:18:19 +0200

    install_clang.sh: use cmake to configure
    autotools does not include the clang-tools
    
    Signed-off-by: Martin Zenzes <martin.zenzes@dfki.de>

--- install_clang.sh
+++ install_clang.sh
@@ -23,11 +23,20 @@ git clone ${llvm_org}/clang-tools-extra.git ${git_location}/tools/clang/tools/ex
 
 mkdir -p ${build_location} && cd ${build_location}
 
-${git_location}/configure \
-    --prefix=${install_location} \
-    --enable-optimized \
-    --disable-assertions \
-    --enable-targets=host-only
+# note that using autotools won't include directories added in
+# "tools/clang/tools/extra/CMakeLists.txt"...? well...
+
+# ${git_location}/configure \
+#     --prefix=${install_location} \
+#     --enable-optimized \
+#     --disable-assertions \
+#     --enable-targets=x86,x86_64,arm
+
+cmake ${git_location}\
+    -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON \
+    -DCMAKE_INSTALL_PREFIX:PATH="$HOME/llvm.install" \
+    -DLLVM_TARGETS_TO_BUILD="X86;AArch64;ARM"
+    -DCMAKE_BUILD_TYPE="Release"
 
 make -j`getconf _NPROCESSORS_ONLN` all check-all install
 
