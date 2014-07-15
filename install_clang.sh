337c44c2ec01454adda1a4af99d288723d218e9a
Martin Zenzes <martin.zenzes@dfki.de>
2014-07-15 11:01:39 +0200

    shell-script to clone, compile and install llvm+clang+tooling

---
+++ install_clang.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+branch=${1:-"release_34"}
+git_location=${2:-"$HOME/llvm.git"}
+install_location=${2:-"$HOME/llvm.install"}
+
+# make the path absolute
+git_location=$(readlink -f ${git_location})
+install_location=$(readlink -f ${install_location})
+
+# some more locations
+build_location="${git_location}/build"
+
+# save typing
+llvm_org="http://llvm.org/git"
+
+git clone ${llvm_org}/llvm.git ${git_location} --branch $branch
+git clone ${llvm_org}/clang.git ${git_location}/tools/clang --branch $branch
+git clone ${llvm_org}/clang-tools-extra.git ${git_location}/tools/clang/tools/extra --branch $branch
+
+mkdir -p ${build_location} && cd ${build_location}
+
+${git_location}/configure \
+    --prefix=${install_location} \
+    --enable-optimized \
+    --disable-assertions \
+    --enable-targets=host-only
+
+make -j`getconf _NPROCESSORS_ONLN` all check-all install
