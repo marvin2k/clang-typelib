21a493d73b35e1e13b78c416e9704dc06e894beb
Janosch Machowinski <janosch.machowinski@dfki.de>
2014-07-16 16:40:22 +0200

    Added code for extraction of size of the data types and offsets of fields

--- extractor/Extractor.cpp
+++ extractor/Extractor.cpp
@@ -26,6 +26,8 @@
 #include "clang/ASTMatchers/ASTMatchFinder.h"
 #include "clang/Basic/SourceManager.h"
 #include "clang/Frontend/FrontendActions.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/AST/RecordLayout.h"
 #include "clang/Lex/Lexer.h"
 #include "clang/Tooling/CompilationDatabase.h"
 #include "clang/Tooling/Refactoring.h"
@@ -68,6 +70,43 @@ class ToolTemplateCallback : public MatchFinder::MatchCallback {
             return;
         }
 
+        bool hasLayout = !D->isDependentType() && !D->isInvalidDecl();
+
+//         std::cout << "Definition " << D->getDefinition() << std::endl;
+
+        if (hasLayout && D->getDefinition())
+        {
+//             std::cout << "Has ASTContext " << &(D->getASTContext()) << std::endl;
+//             
+            const clang::ASTRecordLayout *layout = 0;
+            layout = &(D->getASTContext().getASTRecordLayout(D));
+//             std::cout << "Has layout " << layout << std::endl;
+//             std::cout << "Alignment " << layout-> getAlignment().getQuantity() << std::endl;
+            std::cout << "Type " << D->getQualifiedNameAsString() <<std::endl;
+            std::cout << "  Size" << layout->getSize().getQuantity() << std::endl;
+
+            if(D->field_begin() != D->field_end())
+            {
+                std::cout << "  Members :" << std::endl;
+            }
+            
+            for(RecordDecl::field_iterator fit = D->field_begin(); fit != D->field_end(); fit++)
+            {
+                std::cout << "    Field " << fit->getQualifiedNameAsString() << std::endl;
+                std::cout << "    Offset " << layout->getFieldOffset(fit->getFieldIndex()) << std::endl;
+                std::cout << "    Decl Name " << fit->getNameAsString() << std::endl;
+                
+                SplitQualType T_split = fit->getType().split();
+                std::cout << "    Qual Type " << QualType::getAsString(T_split) << std::endl;
+                std::cout << std::endl;
+            }
+        }
+    
+      std::cout << "got "
+          << D->getTagKind() << " named "
+          << D->getQualifiedNameAsString() << " in "
+          << D->getLocation().printToString(*Result.SourceManager) << "\n";
+        
       std::cout << "got "
           << "'" << clang::TypeWithKeyword::getTagTypeKindName(D->getTagKind()) << "'"
           << " named "
