3e63bb70053c6d76ef011c9c5ed1216c01129b5d
Martin Zenzes <martin.zenzes@dfki.de>
2014-07-18 15:11:11 +0200

    more examples
    Signed-off-by: Martin Zenzes <martin.zenzes@dfki.de>

--- test-header-data/typedefs.h
+++ test-header-data/typedefs.h
@@ -1,21 +1,30 @@
 #ifndef TEST_HEADER_DATA_TYPEDEFS_H
 #define TEST_HEADER_DATA_TYPEDEFS_H
 
+#include <stdint.h>
+
+// what should be done here:
+// 
+// - we want to be able to use a typedef'ed type (intTypedef, intTypedefTypedef and cClassTypedef)
+//   inside a record
+//
+
 namespace ns {
 
-    typedef int intTypedef;
-    typedef intTypedef intTypedefTypedef;
-
-    class aClass {
-        int A;
-    };
-
+    // a class which is typedefed
+    class aClass { int A; };
     typedef aClass aClassTypedef;
 
+    // a simple typedef
+    typedef int intTypedef;
+    // a two-level typedef
+    typedef intTypedef intTypedefTypedef;
+
+    // the struct-struct
     struct test {
+        uint8_t C;
         aClassTypedef A;
         intTypedef B;
-        char C;
         intTypedefTypedef D;
     };
 }
